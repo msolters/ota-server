e55bcb451c48a93fea9e77edc051135a5c63f348
Mark Solters <msolters@gmail.com>
2016-05-26 23:56:49 -0400

    Can now properly send binary data via GET requests

--- ota-server.js
+++ ota-server.js
@@ -1,16 +1,19 @@
 var http = require('http');
 
+host = "::1";
+port = 3003;
+
 var server = http.createServer();
 
 server.on('request', function(req, res) {
   console.log( req );
   res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
-  var bin_data = new Uint8Array("hello world");
-  res.end(bin_data);
+  var bin_data = new Uint8Array([0, 80, 0, 32]); // this will be our .bin file
+  res.end( new Buffer(bin_data, "binary") );
 });
 
 server.on('listening', function() {
   console.log("OTA server listening on http://" + this.address().address + ":" + this.address().port + "/");
 });
 
-server.listen(3003, 'bbbb::1');
+server.listen(port, host);
