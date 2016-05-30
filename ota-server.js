17ef314a7ae2a95fcfe58a192311dfeb728ab5f5
Mark Solters <msolters@driblet.io>
2016-05-30 17:49:11 -0400

    Add more comments

--- ota-server.js
+++ ota-server.js
@@ -3,15 +3,17 @@ var url = require('url');
 var fs = require('fs');
 
 var server = http.createServer();
+//  Our OTA server will use IPv6 address bbbb::1, port 3003
+//  Feel free to use any IPv4 or IPv6 address that your nodes can resolve!
 host = "bbbb::1";
 port = 3003;
 
+//  Read the OTA binary into a buffer to be served on request
 var firmware_binary = fs.readFileSync('ota-image-example.bin');
-//console.log( firmware_binary.toString('hex') );
-//console.log( firmware_binary.length );
 
+//  Here's where we process HTTP requests for chunks of the firmware_binary
 server.on('request', function(req, res) {
-  // (1) Parse request arguments
+  // (1) Parse URL path to obtain the data_start and data_length parameters
   request_parts = url.parse( req.url );
   path_arguments = request_parts.path.split("/");
   var data_start_position = parseInt( path_arguments[1] );
@@ -19,12 +21,12 @@ server.on('request', function(req, res) {
   console.log( "Requesting data:\t" + data_start_position + "\t" + (data_start_position+data_length) );
 
   if (data_start_position >= firmware_binary.length) {
-    //  If there's no more firmware, just send back an empty page (0xff)!
+    //  If there's no more firmware, send back an "EOF" message
     console.log("\tFirmware Binary: Reached End Of File.");
     res.writeHead( 200, {'Content-Type': 'text/plain'} );
     res.end("EOF");
   } else {
-    //  Make sure we don't read past the end of the firmware.
+    //  Make sure we don't read past the end of the firmware binary buffer:
     var data_end_position = Math.min( (data_start_position + data_length), firmware_binary.length );
     data = firmware_binary.slice( data_start_position, data_end_position );
     console.log("\tSending " + data.length + " bytes.");
