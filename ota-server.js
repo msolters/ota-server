e553cba03d14732d1f3cb7639e14a1b606ce1aa6
Mark Solters <msolters@driblet.io>
2016-06-01 00:58:25 -0400

    Firmware now defined using JSON object
    Metadata is automatically generated by parsing the JSON

--- ota-server.js
+++ ota-server.js
@@ -8,8 +8,12 @@ var server = http.createServer();
 host = "bbbb::1";
 port = 3003;
 
-//  Read the OTA binary into a buffer to be served on request
-var firmware_binary = fs.readFileSync('ota-image-example.bin');
+//  Configure the firmware to be served OTA
+var firmware = {
+  binary: fs.readFileSync('ota-image-example.bin'),
+  version: 0x1,
+  uuid: 0xabcd
+};_binary = 
 
 //  Here's where we process HTTP requests for chunks of the firmware_binary
 server.on('request', function(req, res) {
@@ -21,12 +25,11 @@ server.on('request', function(req, res) {
     metadata = new Buffer( 16 );
     metadata.writeUInt16LE( 0x0000, 0 ); // crc
     metadata.writeUInt16LE( 0x0000, 2 ); // crc shadow
-    metadata.writeUInt32LE( firmware_binary.length, 4 ); // binary size
-    metadata.writeUInt16LE( 0x1, 12 ); // version
-    metadata.writeUInt32LE( 0xabcd, 8 ); // uuid
-    metadata.writeUInt16LE( 0x0, 14 ); // 4-byte alignment
+    metadata.writeUInt32LE( firmware.binary.length, 4 ); // binary size
+    metadata.writeUInt32LE( firmware.uuid, 8 ); // uuid
+    metadata.writeUInt16LE( firmware.version, 12 ); // version
+    metadata.writeUInt16LE( 0x0, 14 ); // 4-byte alignment padding
     
-    console.log( metadata );
     res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
     res.end( metadata );
   } else {
@@ -34,15 +37,15 @@ server.on('request', function(req, res) {
     var data_length = parseInt( path_arguments[2] );
     console.log( "Requesting data:\t" + data_start_position + "\t" + (data_start_position+data_length) );
 
-    if (data_start_position >= firmware_binary.length) {
+    if (data_start_position >= firmware.binary.length) {
       //  If there's no more firmware, just send back an empty page (0xff)!
       console.log("\tFirmware Binary: Reached End Of File.");
       res.writeHead( 200, {'Content-Type': 'text/plain'} );
       res.end("EOF");
     } else {
       //  Make sure we don't read past the end of the firmware.
-      var data_end_position = Math.min( (data_start_position + data_length), firmware_binary.length );
-      data = firmware_binary.slice( data_start_position, data_end_position );
+      var data_end_position = Math.min( (data_start_position + data_length), firmware.binary.length );
+      data = firmware.binary.slice( data_start_position, data_end_position );
       console.log("\tSending " + data.length + " bytes.");
       res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
       res.end( data );
