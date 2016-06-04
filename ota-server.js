1e5f347d91e7463c0f9ada2faaa6a9ee4eda5a5d
Mark Solters <msolters@gmail.com>
2016-06-04 15:48:07 -0400

    Removed /metadata URL parsing, assume metadata is embedded into firmware .bin file.

--- ota-server.js
+++ ota-server.js
@@ -9,48 +9,29 @@ host = "bbbb::1";
 port = 3003;
 
 //  Configure the firmware to be served OTA
-var firmware = {
-  binary: fs.readFileSync( process.argv[2] ),
-  version: parseInt( process.argv[3] ),
-  uuid: parseInt( process.argv[4] ),
-  crc: parseInt( process.argv[5] )
-};
+var firmware_binary = fs.readFileSync( process.argv[2] );
 
 //  Here's where we process HTTP requests for chunks of the firmware_binary
 server.on('request', function(req, res) {
   // (1) Parse URL path to obtain the data_start and data_length parameters
   request_parts = url.parse( req.url );
   path_arguments = request_parts.path.split("/");
+  var data_start_position = parseInt( path_arguments[1] );
+  var data_length = parseInt( path_arguments[2] );
+  console.log( "Requesting data:\t" + data_start_position + "\t" + (data_start_position+data_length) );
 
-  if ( path_arguments[1] == "metadata" ) {
-    metadata = new Buffer( 16 );
-    metadata.writeUInt16LE( firmware.crc, 0 ); // crc
-    metadata.writeUInt16LE( 0x0000, 2 ); // crc shadow - this will be calculated by the node
-    metadata.writeUInt32LE( firmware.binary.length, 4 ); // binary size
-    metadata.writeUInt32LE( firmware.uuid, 8 ); // uuid
-    metadata.writeUInt16LE( firmware.version, 12 ); // version
-    metadata.writeUInt16LE( 0x0, 14 ); // 4-byte alignment padding
-
-    res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
-    res.end( metadata );
+  if (data_start_position >= firmware_binary.length) {
+    //  If there's no more firmware, just send back an EOF message!
+    console.log("\tFirmware Binary: Reached End Of File.");
+    res.writeHead( 200, {'Content-Type': 'text/plain'} );
+    res.end("EOF");
   } else {
-    var data_start_position = parseInt( path_arguments[1] );
-    var data_length = parseInt( path_arguments[2] );
-    console.log( "Requesting data:\t" + data_start_position + "\t" + (data_start_position+data_length) );
-
-    if (data_start_position >= firmware.binary.length) {
-      //  If there's no more firmware, just send back an empty page (0xff)!
-      console.log("\tFirmware Binary: Reached End Of File.");
-      res.writeHead( 200, {'Content-Type': 'text/plain'} );
-      res.end("EOF");
-    } else {
-      //  Make sure we don't read past the end of the firmware.
-      var data_end_position = Math.min( (data_start_position + data_length), firmware.binary.length );
-      data = firmware.binary.slice( data_start_position, data_end_position );
-      console.log("\tSending " + data.length + " bytes.");
-      res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
-      res.end( data );
-    }
+    //  Make sure we don't read past the end of the firmware.
+    var data_end_position = Math.min( (data_start_position + data_length), firmware_binary.length );
+    data = firmware_binary.slice( data_start_position, data_end_position );
+    console.log("\tSending " + data.length + " bytes.");
+    res.writeHead( 200, {'Content-Type': 'application/octet-stream'} );
+    res.end( data );
   }
 });
 
